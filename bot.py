2ba6927ca6494a729f54047c1da713be9d6a1795
LSouzaAndrade <lucas.souza.andrade66@gmail.com>
2024-11-08 16:06:07 -0300

    Teste de processamento POST

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import uvicorn
 from discord.ext import commands
 from dotenv import load_dotenv
 from fastapi import FastAPI
-from pydantic import BaseModel
+from fuzzywuzzy import fuzz
 
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
@@ -21,11 +21,6 @@ app = FastAPI()
 config = uvicorn.Config(app, host="0.0.0.0", port=8000)
 server = uvicorn.Server(config)
 
-class ActionRequest(BaseModel):
-    guild_id: int
-    member_id: int
-    target_channel_id: int = None
-
 @bot.event
 async def on_ready():
     print(f'Aplicação conectada como {bot.user}.')
@@ -33,21 +28,33 @@ async def on_ready():
 def get_status():
     status = {}
     for guild in bot.guilds:
-        guild_info = {
-            "nome_servidor": guild.name,
-            "canais_de_voz": {}
-        }
         for channel in guild.voice_channels:
-            channel_info = {
-                "nome_canal": channel.name,
-                "membros_conectados": {}
-            }
             for member in channel.members:
-                channel_info["membros_conectados"][str(member.id)] = member.display_name
+                member_info = {
+                    "display_name": member.display_name,
+                    "voice_channel_id": str(channel.id),
+                    "guild_id": str(guild.id)
+                }
+                status[str(member.id)] = member_info
+    return status
 
-            guild_info["canais_de_voz"][str(channel.id)] = channel_info
-        status[str(guild.id)] = guild_info
-    print(str(status))
+def get_user_id_by_nick(status, nick):
+    for user_id, user_info in status.items():
+        if user_info["display_name"] == nick:
+            return user_id
+    return None
+
+def get_guild_id_by_nick(status, nick):
+    for user_info in status.values():
+        if user_info["display_name"] == nick:
+            return user_info["guild_id"]
+    return None
+
+def fuzzy_analysis(status, heard_nickname):
+    online_nicknames = [user_info["display_name"] for user_info in status.values()]
+    matches = [(nome, fuzz.ratio(heard_nickname, nome)) for nome in online_nicknames]
+    filtered_matches = [match for match in matches if match[1] >= 65]
+    return filtered_matches
 
 async def disconnect_user(guild_id: int, member_id: int):
     guild = bot.get_guild(guild_id)
@@ -86,14 +93,21 @@ async def move_user(guild_id: int, member_id: int, target_channel_id: int):
         return f'Servidor {guild_id} não encontrado.'
 
 @app.post("/disconnect_user/")
-async def disconnect_user_endpoint(action: ActionRequest):
-    response = await disconnect_user(action.guild_id, action.member_id)
+async def disconnect_user_endpoint(payload):
+    print(str(payload))
+    status = get_status()
+    filtered_nicks = fuzzy_analysis(status, payload.member_nick)
+    server_id = get_guild_id_by_nick(status, filtered_nicks[0][0])
+    member_id = get_user_id_by_nick(status, filtered_nicks[0][0])
+    response = await disconnect_user(server_id, member_id)
     return {"message": response}
 
 @app.post("/move_user/")
-async def move_user_endpoint(action: ActionRequest):
-    if action.target_channel_id:
-        response = await move_user(action.guild_id, action.member_id, action.target_channel_id)
+async def move_user_endpoint(payload):
+    if payload.target_channel_id:
+        status = get_status()
+        nicks = [user_info["display_name"] for user_info in status.values()]
+        response = await move_user(payload.guild_id, payload.member_id, payload.target_channel_id)
         return {"message": response}
     else:
         return {"error": "É necessário fornecer o ID do canal de destino"}
